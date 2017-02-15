ae5e175cdd0f073e4abfe315dfb1f531c7b2cef8
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2017-02-15 11:34:18 +0000

    Added source and issues url

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ license 'Apache 2.0'
 name 'start-stop-daemon'
 description 'Installs start-stop-daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/EagleGenomics-cookbooks/start-stop-daemon'
+issues_url 'https://github.com/EagleGenomics-cookbooks/start-stop-daemon/issues'
 version '1.1.4'
 
 recipe 'start-stop-daemon', 'Installs start-stop-daemon to any Linux distribution'
